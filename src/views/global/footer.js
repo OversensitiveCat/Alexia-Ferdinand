e477badab988d14d931bf2b30c6c22cdb52c0897
OversensitiveCat <124178833+OversensitiveCat@users.noreply.github.com>
2023-03-04 20:05:20 +0100

    adapt footer on mobile

--- src/views/global/footer.js
+++ src/views/global/footer.js
@@ -6,52 +6,68 @@ gsap.registerPlugin(ScrollTrigger)
 const footer = (data) => {
   function footerAnim() {
     if (data.next.namespace != 'home') {
-      let mailLink = document.querySelector('.mail-link')
-      gsap.set(mailLink, { autoAlpha: 1 })
-
-      let tl = gsap.timeline({ paused: true })
-      tl.from(mailLink, { autoAlpha: 0, yPercent: 75, duration: 0.75 }).from(
-        '.flower-mail',
-        { rotate: -90, transformOrigin: 'center', duration: 0.75 },
-        '<'
-      )
-
-      ScrollTrigger.create({
-        trigger: '.card-background',
-        start: 'bottom bottom',
-        onLeaveBack: () => {
-          tl.progress(0)
-          tl.pause()
+      let mm = gsap.matchMedia()
+      mm.add(
+        {
+          isDesktop: `(min-width: 992px)`,
+          isMobile: `(max-width: 991px)`,
         },
-      })
+        (context) => {
+          let { isDesktop, isMobile } = context.conditions
 
-      ScrollTrigger.create({
-        trigger: '.card-background',
-        start: 'bottom 75%',
-        onEnter: () => tl.play(),
-      })
+          let mailLink = document.querySelector('.mail-link')
 
-      let tlMail = gsap.timeline({ paused: true })
-      tlMail
-        .to('.mail-underline', {
-          width: '100%',
-          duration: 0.5,
-          ease: 'power4.in',
-        })
-        .to(
-          '.flower-mail',
-          {
-            scale: 1.2,
-            rotate: 90,
-            transformOrigin: 'center',
-            duration: 0.5,
-            ease: 'power1.in',
-          },
-          '<'
-        )
+          let tl = gsap.timeline({ paused: true })
+          tl.from(mailLink, {
+            autoAlpha: 0,
+            yPercent: isMobile ? 25 : 50,
+            duration: 0.75,
+          }).from(
+            '.flower-mail',
+            { rotate: -90, transformOrigin: 'center', duration: 0.75 },
+            '<'
+          )
 
-      mailLink.addEventListener('mouseenter', () => tlMail.play())
-      mailLink.addEventListener('mouseleave', () => tlMail.reverse())
+          ScrollTrigger.create({
+            trigger: '.card-background',
+            start: 'bottom bottom',
+            onLeaveBack: () => {
+              tl.progress(0)
+              tl.pause()
+            },
+          })
+
+          ScrollTrigger.create({
+            trigger: '.card-background',
+            start: 'bottom 75%',
+            onEnter: () => tl.play(),
+          })
+
+          if (isDesktop) {
+            let tlMail = gsap.timeline({ paused: true })
+            tlMail
+              .to('.mail-underline', {
+                width: '100%',
+                duration: 0.5,
+                ease: 'power4.in',
+              })
+              .to(
+                '.flower-mail',
+                {
+                  scale: 1.2,
+                  rotate: 90,
+                  transformOrigin: 'center',
+                  duration: 0.5,
+                  ease: 'power1.in',
+                },
+                '<'
+              )
+
+            mailLink.addEventListener('mouseenter', () => tlMail.play())
+            mailLink.addEventListener('mouseleave', () => tlMail.reverse())
+          }
+        }
+      )
     } else return
   }
 
